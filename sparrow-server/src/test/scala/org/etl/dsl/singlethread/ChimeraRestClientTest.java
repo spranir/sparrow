b683db21293ebb1ff70d541f268f2b7fc4f46865
( -_- ) <( -_- )@192.168.1.9>
2017-10-02 15:44:29 +0530

    New changes are added

---
+++ sparrow-server/src/test/scala/org/etl/dsl/singlethread/ChimeraRestClientTest.java
@@ -0,0 +1,29 @@
+package org.etl.dsl.singlethread;
+import java.io.IOException;
+
+
+import org.junit.Test;
+
+import in.chimera.httpclient.ChimeraRestClient;
+
+
+
+public class ChimeraRestClientTest {
+
+	@Test
+	public void testPOST() {
+		String baseURL="https://lab.lasalle.in/fineract-provider/api/v1";
+		String userName = "Mifos";
+		String password = "duruv@@639001";
+		String tenantId = "default";
+		String path = "/datatables/CollectionDenomination/312?genericResultSet=true";
+		String payload ="{\"Denomination\":\"2000\",\"NoteValue\":\"5\",\"Value\":\"100000\",\"CollectionDate\":\"26 Sep 2017\",\"CollectionTime\":\"1345\",\"GroupLoanId\":\"0987\",\"locale\":\"en\",\"dateFormat\":\"dd MMMM yyyy\"}";
+		ChimeraRestClient client = new ChimeraRestClient(baseURL, "Mifos:duruv@@639001:chimeraauth:default");
+		client.createAuthToken();
+		client.post(path, payload);
+		
+		
+	}
+	
+	
+}
